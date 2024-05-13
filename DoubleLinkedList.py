05b3f74f7fd80d5870939182811d4dfcadc0b56c
R3Z <rogerjohnmorellizeller@gmail.com>
2024-05-13 10:52:48 -0400

    added DoubleLinkedList and Node files

---
+++ DoubleLinkedList.py
@@ -0,0 +1,20 @@
+import Node
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head_node = None
+        self.tail_node = None
+
+    def add_to_head(self, new_value):
+        new_head = Node(new_value)
+        current_head = Node(self.head_node)
+
+        if current_head != None:
+            current_head.set_prev_node(new_head)
+            new_head.set_next_node(current_head)
+
+        self.head_node = new_head
+
+        if self.tail_node == None:
+            self.tail_node = new_head
