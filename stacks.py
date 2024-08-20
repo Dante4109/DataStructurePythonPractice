f15ea1937d6cf97f2fc19aaa085fc7628d8b46aa
R3Z <rogerjohnmorellizeller@gmail.com>
2024-08-19 21:45:08 -0400

    completed course on stacks

--- stacks.py
+++ stacks.py
@@ -2,9 +2,63 @@ from node import Node
 
 
 class Stack:
-    def __init__(self):
+    def __init__(self, limit=1000):
         self.top_item = None
-        pass
+        self.size = 0
+        self.limit = limit
 
     def peek(self):
-        return self.top_item.value
+        if not self.is_empty():
+            return self.top_item.get_value()
+        else:
+            print("The stack is empty!")
+
+    def push(self, value):
+        if self.has_space():
+            item = Node(value)
+            item.set_next_node(self.top_item)
+            self.top_item = item
+            self.size += 1
+        else:
+            print("Stack size limit reached!")
+
+    def pop(self):
+        if not self.is_empty():
+            item_to_remove = self.top_item
+            self.top_item = item_to_remove.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
+        else:
+            print("The stack is empty!")
+
+    def has_space(self):
+        return self.limit > self.size
+
+    def is_empty(self):
+        return self.size == 0
+
+
+# Defining an empty pizza stack
+pizza_stack = Stack(6)
+# Adding pizzas as they are ready until we have
+pizza_stack.push("pizza #1")
+pizza_stack.push("pizza #2")
+pizza_stack.push("pizza #3")
+pizza_stack.push("pizza #4")
+pizza_stack.push("pizza #5")
+pizza_stack.push("pizza #6")
+
+# Uncomment the push() statement below:
+pizza_stack.push("pizza #7")
+
+# Delivering pizzas from the top of the stack down
+print("The first pizza to deliver is " + str(pizza_stack.peek()))
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+
+# Uncomment the pop() statement below:
+pizza_stack.pop()
